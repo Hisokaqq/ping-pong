2aa4011fe210416a6503b15fb7e23c601ed3b151
Alex <baklan4ik1993@gmail.com>
2023-03-15 14:08:08 +0100

    completed

--- App.js
+++ App.js
@@ -1,52 +1,233 @@
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import { StyleSheet, Text, useWindowDimensions, View,  } from 'react-native';
-import Animated, {Easing, useAnimatedStyle, useSharedValue, withTiming} from 'react-native-reanimated';
+import React, { useState } from "react";
+import { useEffect } from "react";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  useWindowDimensions,
+  View,
+} from "react-native";
+import Animated, {
+  Easing,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+  useAnimatedGestureHandler,
+  withSequence,
+  BounceIn,
+} from "react-native-reanimated";
+import {
+  PanGestureHandler,
+} from "react-native-gesture-handler";
 
-export default function App() {
-  const normVec = vector => {
-    const magnitude = Math.sqrt(vector.x * vector.x + vector.y * vector.y)
-    return ({
-      x: vector.x/magnitude,
-      y: vector.y/magnitude
-    })
-  }
-  const targetPositionX = useSharedValue(150)
-  const targetPositionY = useSharedValue(150)
-  const direction = useSharedValue(normVec({x: Math.random(), y: Math.random() }))
-  const {height, width} = useWindowDimensions()
-  useEffect(()=>{
-    const interval = setInterval(update, DELTA)
-    return ()=>clearInterval(interval)
-  },[])
-  const FPS = 60
-  const DELTA = 1000/FPS
-  const SPEED = 10
-  const islandDim = {x:100, y:300, width: 100, height: 20}
-  const update = () =>{
-    const nextX = targetPositionX.value+ direction.value.x * SPEED
-    const nextY = targetPositionY.value+ direction.value.y * SPEED
-    if (nextY<0 || nextY + 25 >height){
-      direction.value = {x: direction.value.x, y: -direction.value.y}
+const FPS = 60;
+const DELTA = 1000 / FPS;
+const SPEED = 10;
+const BALL_WIDTH = 25;
+
+const islandDimensions = { x: 150, y: 11, w: 127, h: 37 };
+
+const normalizeVector = (vector) => {
+  const magnitude = Math.sqrt(vector.x * vector.x + vector.y * vector.y);
+
+  return {
+    x: vector.x / magnitude,
+    y: vector.y / magnitude,
+  };
+};
+
+export default function Game() {
+  const { height, width } = useWindowDimensions();
+  const playerDimensions = {
+    w: width / 2,
+    h: 37,
+  };
+
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  const targetPositionX = useSharedValue(width / 2);
+  const targetPositionY = useSharedValue(height / 2);
+  const direction = useSharedValue(
+    normalizeVector({ x: Math.random(), y: Math.random() })
+  );
+  const playerPos = useSharedValue({ x: width / 4, y: height - 100 });
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (!gameOver) {
+        update();
+      }
+    }, DELTA);
+
+    return () => clearInterval(interval);
+  }, [gameOver]);
+
+  const update = () => {
+    let nextPos = getNextPos(direction.value);
+    let newDirection = direction.value;
+
+    // Wall Hit detection
+    if (nextPos.y > height - BALL_WIDTH) {
+      setGameOver(true);
     }
-    if (nextX<0 || nextX + 25>width){
-      direction.value = {x: -direction.value.x, y: direction.value.y}
+    if (nextPos.y < 0) {
+      newDirection = { x: direction.value.x, y: -direction.value.y };
     }
-    targetPositionX.value = withTiming(nextX, {duration: DELTA, easing: Easing.linear})
-    targetPositionY.value = withTiming(nextY, {duration: DELTA, easing: Easing.linear})
-    
-  }
-  const ballAnimatedStyles = useAnimatedStyle(()=>{
+
+    if (nextPos.x < 0 || nextPos.x > width - BALL_WIDTH) {
+      newDirection = { x: -direction.value.x, y: direction.value.y };
+    }
+
+    // Island Hit detection
+    if (
+      nextPos.x < islandDimensions.x + islandDimensions.w &&
+      nextPos.x + BALL_WIDTH > islandDimensions.x &&
+      nextPos.y < islandDimensions.y + islandDimensions.h &&
+      BALL_WIDTH + nextPos.y > islandDimensions.y
+    ) {
+      if (
+        targetPositionX.value < islandDimensions.x ||
+        targetPositionX.value > islandDimensions.x + islandDimensions.w
+      ) {
+        newDirection = { x: -direction.value.x, y: direction.value.y };
+      } else {
+        newDirection = { x: direction.value.x, y: -direction.value.y };
+      }
+      setScore((s) => s + 1);
+    }
+
+    // Player Hit detection
+    if (
+      nextPos.x < playerPos.value.x + playerDimensions.w &&
+      nextPos.x + BALL_WIDTH > playerPos.value.x &&
+      nextPos.y < playerPos.value.y + playerDimensions.h &&
+      BALL_WIDTH + nextPos.y > playerPos.value.y
+    ) {
+      if (
+        targetPositionX.value < playerPos.value.x ||
+        targetPositionX.value > playerPos.value.x + playerDimensions.w
+      ) {
+        newDirection = { x: -direction.value.x, y: direction.value.y };
+      } else {
+        newDirection = { x: direction.value.x, y: -direction.value.y };
+      }
+    }
+
+    direction.value = newDirection;
+    nextPos = getNextPos(newDirection);
+
+    targetPositionX.value = withTiming(nextPos.x, {
+      duration: DELTA,
+      easing: Easing.linear,
+    });
+    targetPositionY.value = withTiming(nextPos.y, {
+      duration: DELTA,
+      easing: Easing.linear,
+    });
+  };
+
+  const getNextPos = (direction) => {
     return {
-      top: targetPositionY.value, 
+      x: targetPositionX.value + direction.x * SPEED,
+      y: targetPositionY.value + direction.y * SPEED,
+    };
+  };
+
+  const restartGame = () => {
+    targetPositionX.value = width / 2;
+    targetPositionY.value = height / 2;
+    setScore(0);
+    setGameOver(false);
+  };
+
+  const ballAnimatedStyles = useAnimatedStyle(() => {
+    return {
+      top: targetPositionY.value,
       left: targetPositionX.value,
-    }
-  })
+    };
+  });
+
+  const islandAnimatedStyles = useAnimatedStyle(
+    () => ({
+      width: withSequence(
+        withTiming(islandDimensions.w * 1.3),
+        withTiming(islandDimensions.w)
+      ),
+      height: withSequence(
+        withTiming(islandDimensions.h * 1.3),
+        withTiming(islandDimensions.h)
+      ),
+      opacity: withSequence(withTiming(0), withTiming(1)),
+    }),
+    [score]
+  );
+
+  const playerAnimatedStyles = useAnimatedStyle(() => ({
+    left: playerPos.value.x,
+  }));
+
+  const gestureHandler = useAnimatedGestureHandler({
+    onActive: (event) => {
+      playerPos.value = {
+        ...playerPos.value,
+        x: event.absoluteX - playerDimensions.w / 2,
+      };
+    },
+  });
+
   return (
     <View style={styles.container}>
-      <Animated.View style={[styles.ball, ballAnimatedStyles]}/>
-      <View style={{top: islandDim.y, left: islandDim.x, width: islandDim.width, height: islandDim.height, backgroundColor: "#000"}}/>
-      <StatusBar style="auto" />
+      <Text style={styles.score}>{score}</Text>
+      {gameOver && (
+        <View style={styles.gameOverContainer}>
+          <Text style={styles.gameOver}>Game over</Text>
+          <Button title="Restart" onPress={restartGame} />
+        </View>
+      )}
+
+      {!gameOver && <Animated.View style={[styles.ball, ballAnimatedStyles]} />}
+
+      {/* Island */}
+      <Animated.View
+        entering={BounceIn}
+        key={score}
+        style={{
+          position: "absolute",
+          top: islandDimensions.y,
+          left: islandDimensions.x,
+          width: islandDimensions.w,
+          height: islandDimensions.h,
+          borderRadius: 20,
+          backgroundColor: "black",
+        }}
+      />
+
+      {/* Player */}
+      <Animated.View
+        style={[
+          {
+            top: playerPos.value.y,
+            position: "absolute",
+            width: playerDimensions.w,
+            height: playerDimensions.h,
+            borderRadius: 20,
+            backgroundColor: "black",
+          },
+          playerAnimatedStyles,
+        ]}
+      />
+
+      <PanGestureHandler onGestureEvent={gestureHandler}>
+        <Animated.View
+          style={{
+            width: "100%",
+            height: 200,
+            position: "absolute",
+            bottom: 0,
+          }}
+        />
+      </PanGestureHandler>
     </View>
   );
 }
@@ -54,21 +235,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
   },
   ball: {
-    width: 25,
+    backgroundColor: "black",
+    width: BALL_WIDTH,
     aspectRatio: 1,
-    borderRadius: "50%",
-    backgroundColor: 'black',
+    borderRadius: 25,
     position: "absolute",
   },
-  // island: {
-  //   width: 100,
-  //   height: 30,
-  //   backgroundColor:  "#000",
-  //   borderRadius: 20,
-  // }
+  score: {
+    fontSize: 150,
+    fontWeight: "500",
+    position: "absolute",
+    top: 150,
+    color: "lightgray",
+  },
+  gameOverContainer: {
+    position: "absolute",
+    top: 350,
+  },
+  gameOver: {
+    fontSize: 50,
+    fontWeight: "500",
+    color: "red",
+  },
 });
